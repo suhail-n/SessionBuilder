5e151380d801786355018146540af2065dadfb49
Suhail-n <suhail.nooristani@gmail.com>
2019-08-22 17:25:41 -0400

    added SessionBuilder core

---
+++ src/SessionBuilder.Core/SessionContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SessionBuilder.Core
+{
+    public class SessionContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source='sesson.db'");
+        }
+
+    }
+}
